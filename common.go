a6de2e5b06327f76e4687407e2c96c50e4f4d82d
John Martinez <jvmatl@gmail.com>
2022-09-08 13:14:55 -0400

    Update common.go
    reduce max plaintext (1024) and max ciphertext (2048) buffer sizes to work around a broken embedded TLS stack we need to communicate with. 
    
    This WILL negatively impact performance (and might not even work) -- use at your own risk.

--- common.go
+++ common.go
@@ -34,9 +34,9 @@ const (
 )
 
 const (
-	maxPlaintext       = 16384        // maximum plaintext payload length
-	maxCiphertext      = 16384 + 2048 // maximum ciphertext payload length
-	maxCiphertextTLS13 = 16384 + 256  // maximum ciphertext length in TLS 1.3
+	maxPlaintext       = 1024        // maximum plaintext payload length
+	maxCiphertext      = maxPlaintext + 1024 // maximum ciphertext payload length
+	maxCiphertextTLS13 = maxPlaintext + 256  // maximum ciphertext length in TLS 1.3
 	recordHeaderLen    = 5            // record header length
 	maxHandshake       = 65536        // maximum handshake we support (protocol max is 16 MB)
 	maxUselessRecords  = 16           // maximum number of consecutive non-advancing records
